b1c7db8b9f40b6b6dd0895661b96659afab3947f
Densi10 <118915327+Densi10@users.noreply.github.com>
2023-10-13 12:26:38 +0000

    conversion of datatypes

---
+++ 01_Basics/conversionOperation.js
@@ -0,0 +1,32 @@
+let score = null;
+// console.log(typeof score);
+
+let value = Number(score);
+// console.log(typeof value);
+// console.log(value);
+
+// If I convert 
+// 30  => 30
+// "30" => 30
+// "30abc" => NaN
+// "abcd" => NaN
+// true => 1; False => 0
+// null => 0 
+
+let isLoggedIn = "new";
+
+let booleanLoggedIn  = Boolean(isLoggedIn);
+// console.log(booleanLoggedIn);
+
+// if I enter 1 => true
+//   0 => false
+//   "" => false 
+//  "new" => true 
+
+let someNumbher = undefined
+
+let stringNumber = String(someNumbher);
+console.log(typeof stringNumber);
+
+// it will return string datatype in any situation
+
