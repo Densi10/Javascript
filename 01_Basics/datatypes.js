4a5ecced947e266efee259aefe616b95fba956de
Densi10 <118915327+Densi10@users.noreply.github.com>
2023-10-13 11:38:21 +0000

    variables and Datatypes changes

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,21 @@
+"Use strict" ;  // Treat all Js code as Newer Vesrion of Javascript
+
+// alert(3+3)  We are using node , not browser
+
+console.log( 3  
+     + 3 );  // Code readability should be High
+
+console.log("Demo");
+
+/* DATATYPES */
+// number
+//bigInt
+//string
+//null  => object
+//undefined
+//boolean => true , false
+//symbol
+//object
+
+console.log( typeof null);  // Object
+console.log( typeof undefined ); // unjdefined
---
+++ 01_Basics/variables.js
@@ -0,0 +1,22 @@
+const accountId = 1234
+let accountName = "Demo"
+var accountEmail = "demo123@gmail.com"
+accountCity = "surat"
+let accountState;
+
+// accountId = 2   It gives Constant error Constant can't change
+accountName = "newDemo"
+accountEmail = "newD123@gmail.com"
+accountCity = "vadodara"
+
+/* 
+ prefer not to use Var 
+ Because3 of blockScope and function scope Issue 
+*/
+
+/* 
+ Let resolve the issue that occurs while using var 
+  so prefer let for declaring Variables 
+*/
+
+console.table([accountId,accountEmail,accountName,accountCity,accountState]);
