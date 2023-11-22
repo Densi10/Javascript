941df041db398b4202a6e7d760a2d96c3afce8ff
Densi10 <118915327+Densi10@users.noreply.github.com>
2023-11-22 13:24:17 +0000

    datatypes and string

--- 01_Basics/datatypes-summary.js
+++ 01_Basics/datatypes-summary.js
@@ -13,3 +13,28 @@
 //  which means that data types of variables are determined by the value
 //   they hold at runtime and can change throughout the program 
 //   as we assign different values to them. //
+
+//////// MEMORY LOCATION ACCORDING TO DATATYPES //////////
+
+// There are two types of memory :
+
+//  1 . Stack Memory and 2. Heap Memory
+//  =>  primitive     &     Non-primitive
+//  =>  Copy of data  &     Real value reference 
+
+let myName = "DensiSakhreliya"
+let myNewName = myName ;
+ myNewName = "Densi" ;
+
+console.log(myName);
+console.log(myNewName);
+
+let userOne = {
+  email : "123@gmail.com",
+  mobile : "2213456700"
+}
+let userTwo = userOne ;
+userTwo.email = "abc@gmail.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
---
+++ 01_Basics/strings.js
@@ -0,0 +1,21 @@
+const Name = " Densi ";
+const age = 20;
+
+// console.log(`Hello ! My name is ${Name} and My age is ${age}`);
+
+const gameName = 'cunning-Den-10-easy'
+
+// console.log(gameName.length);
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf('g'));
+// console.log(gameName.substring(0,4));
+// const newString = gameName.trim();
+// console.log(gameName);
+// console.log(newString);
+// console.log(gameName.substring(0,4));
+// const url = 'https://localhost:1200%20itshere'
+// const newUrl = url.replace('%20','-')
+// console.log(newUrl);
+// console.log(url.includes("d"));
+
+console.log(gameName.split('-'));
