3e886d807408bcfe5fc26b95168b78e391c52ff6
Densi10 <118915327+Densi10@users.noreply.github.com>
2023-11-23 07:14:50 +0000

    number and maths added

---
+++ 01_Basics/nums_and_maths.js
@@ -0,0 +1,16 @@
+const balance = 1000000;
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+// console.log(balance.toPrecision(2));
+// console.log(balance.toLocaleString('en-IN'));
+
+// Math 
+
+// console.log(Math.random());
+// console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random()*(max-min+1)+min));
