fc3f24b27bb5cc4c5114020bae03dbd3c846f128
Densi10 <118915327+Densi10@users.noreply.github.com>
2023-12-04 12:43:59 +0000

    more method of array

---
+++ 02_Basics/02_arrays.js
@@ -0,0 +1,22 @@
+const marvel = ["thor","ironman","spiderman"];
+const dc =["superman","flash","batman"];
+// marvel.push(dc);
+// console.log(marvel);
+
+// spread operator 
+
+// for merge array
+
+// const allHeros = [...marvel,...dc];
+// console.log(allHeros);
+
+
+// // flat 
+
+// const newArr = [1,2,3,[4,5],8,9,[10,11,6,[8,6,4]]];
+// const new_arr =  newArr.flat(Infinity);
+// console.log(newArr);
+// console.log(new_arr);
+
+console.log(Array.isArray("Densi"));
+console.log(Array.from("Densi")); // converts in to array 
