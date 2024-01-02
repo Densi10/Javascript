d09e7be31bbd21daeb201679abca509c6671c9e6
Densi10 <118915327+Densi10@users.noreply.github.com>
2024-01-02 11:47:33 +0000

    ternary operator

--- 04_Control_flow/truthy.js
+++ 04_Control_flow/truthy.js
@@ -7,13 +7,30 @@
 // 'false' , "0" , " " , [] , {} , function(){}
 
 
-const emptyArray = [];
+// const emptyArray = [];
 
-if(emptyArray.length === 0){
-    console.log("array is empty");
-}
+// if(emptyArray.length === 0){
+//     console.log("array is empty");
+// }
 
-const emptyObject = {}
-if(Object.keys(emptyObject).length === 0){
-    console.log("object is empty");
-}
+// const emptyObject = {}
+// if(Object.keys(emptyObject).length === 0){
+//     console.log("object is empty");
+// }
+
+// Nullish calescing operator (??) :  null undefined 
+
+// let val1;
+// // val1 = 5 ?? 10
+// // val1 = null ?? 10
+// val1 = undefined ?? 15
+
+// console.log(val1);
+
+//// Ternary operator 
+
+// condition : true ? false
+
+const value = 20
+
+value >=10 ? console.log("condition is true") : console.log("condition is false");
