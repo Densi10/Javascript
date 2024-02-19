0128a7f4feedcea2e21b4e671b9b70d710685913
Densi10 <118915327+Densi10@users.noreply.github.com>
2024-02-19 06:49:22 +0000

    continue promises

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -28,3 +28,66 @@ const PromiseThree = new Promise((resolve,reject)=>{
 PromiseThree.then((user)=>{
     console.log(user);
 })
+
+const promiseFour = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        let error = true;
+    if(!error){
+        resolve({username:"Densi",password:"123"})
+    }else{
+        reject("ERROR ! something went wrong...")
+    }
+    },1000);
+});
+
+promiseFour.then((user)=>{
+    console.log(user);
+    return user.username
+}).then((username)=>{
+    console.log(username);
+}).catch((error)=>{
+    console.log(error);
+}).finally(()=>console.log("promise is either resolved or rejected !"))
+
+const promiseFive = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        let error = false;
+        if(!error){
+            resolve({username:"Javascript",password:"1234"})
+        }else{
+            reject("ERROR ! js error printed");
+        }
+    },1000)
+});
+
+async function consumePromiseFive(){
+    try {
+        const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+consumePromiseFive();
+
+// async function getAllUsers(){
+//    try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users');
+//         const data = await response.json();
+//         console.log(data);
+//    } catch (error) {
+//         console.log( error );
+//    }
+// }
+
+// getAllUsers()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response)=>{
+    return response.json()
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch((error) => console.log(error))
